6957942ac45f75273c2da309006aefb95f142ba6
jose-tello <47975174+jose-tello@users.noreply.github.com>
2020-11-07 18:51:44 +0100

    buy info row added

--- prueba1/lib/screens/shop_info_screen.dart
+++ prueba1/lib/screens/shop_info_screen.dart
@@ -6,28 +6,20 @@ class ShopInfoScreen extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: [
-        Expanded(flex: 6, child: _PomegranadePreview()),
-        Expanded(flex: 2, child: _BuyInfo()),
+        Expanded(flex: 8, child: _PomegranadePreview()),
         Expanded(flex: 2, child: _PomegranadeInfo()),
       ],
     ));
   }
 }
 
-
 class _BuyInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-         Container(
-           decoration: BoxDecoration(
-           color: Colors.black,
-           border: Border.all(color: Colors.deepPurple[50], width: 10),
-      ),
-  )
-    ],
-    );
+    return Scaffold(
+        body: Row(
+      children: [],
+    ));
   }
 }
 
@@ -35,27 +27,56 @@ class _PomegranadePreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-          color: Colors.pink,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(25),
-            topRight: Radius.circular(25),
-          )),
-    );
+        child: Stack(
+      children: [
+        Container(
+          height: 500,
+          decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              )),
+        ),
+        Positioned(
+            bottom: 0,
+            left: 50,
+            child: Row(
+              children: [
+                Container(
+                  width: 220,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.deepPurple[200], width: 4),
+                  ),
+                ),
+                 Positioned(
+            bottom: -10,
+            left: 50,
+                child: Container(
+                  width: 150,
+                  height: 90,
+                  color: Colors.green[300],
+                )
+                 )
+              ],
+            ))
+      ],
+    ));
   }
 }
-
 
 class _PomegranadeInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text('UNIT'),
-        ],)
-    );
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('UNIT'),
+          ],
+        ));
   }
 }
