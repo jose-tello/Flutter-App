3a6a2561bfa88e91315b3c919710a6849ae8d7f9
jose-tello <47975174+jose-tello@users.noreply.github.com>
2020-11-07 18:54:48 +0100

    regular commit

--- prueba1/lib/screens/shop_info_screen.dart
+++ prueba1/lib/screens/shop_info_screen.dart
@@ -13,16 +13,6 @@ class ShopInfoScreen extends StatelessWidget {
   }
 }
 
-class _BuyInfo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Row(
-      children: [],
-    ));
-  }
-}
-
 class _PomegranadePreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -38,32 +28,42 @@ class _PomegranadePreview extends StatelessWidget {
                 topRight: Radius.circular(25),
               )),
         ),
-        Positioned(
-            bottom: 0,
-            left: 50,
-            child: Row(
-              children: [
-                Container(
-                  width: 220,
-                  height: 140,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.deepPurple[200], width: 4),
-                  ),
-                ),
-                 Positioned(
-            bottom: -10,
-            left: 50,
+        _BuyInfo()
+      ],
+    ));
+  }
+}
+
+class _BuyInfo extends StatelessWidget {
+  const _BuyInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: 0,
+        left: 50,
+        child: Row(
+          children: [
+            Container(
+              width: 220,
+              height: 140,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.deepPurple[200], width: 4),
+              ),
+            ),
+            Positioned(
+                bottom: -10,
+                left: 50,
                 child: Container(
                   width: 150,
                   height: 90,
                   color: Colors.green[300],
-                )
-                 )
-              ],
-            ))
-      ],
-    ));
+                ))
+          ],
+        ));
   }
 }
 
