cdc8703c9790c9b3db624608f3b4e365f7513165
jose-tello <47975174+jose-tello@users.noreply.github.com>
2020-11-07 13:42:57 +0100

    added widget pomegranade info

--- prueba1/lib/screens/shop_info_screen.dart
+++ prueba1/lib/screens/shop_info_screen.dart
@@ -6,22 +6,39 @@ class ShopInfoScreen extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: [
-        Expanded(
-          flex: 6,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.pink,
-              //borderRadius: 
-            ),
-            
-          ),
-        ),
-
-        Expanded(
-          flex: 3,
-          child: Container(),
-        ),
+        Expanded(flex: 6, child: _PomegranadePreview()),
+        Expanded(flex: 3, child: _PomegranadeInfo()),
       ],
     ));
+  }
+}
+
+
+class _PomegranadePreview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.pink,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          )),
+    );
+  }
+}
+
+
+class _PomegranadeInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('UNIT'),
+        ],)
+    );
   }
 }
