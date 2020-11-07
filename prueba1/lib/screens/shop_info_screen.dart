32aa68f15c4800e6442e2141abdb3d7d6ac34be2
jose-tello <47975174+jose-tello@users.noreply.github.com>
2020-11-07 19:43:41 +0100

    add to cart and buy indo added

--- prueba1/lib/screens/shop_info_screen.dart
+++ prueba1/lib/screens/shop_info_screen.dart
@@ -9,7 +9,6 @@ class ShopInfoScreen extends StatelessWidget {
         body: Column(
       children: [
         Expanded(flex: 6, child: _PomegranadePreview()),
-        Expanded(flex: 2, child: _BuyInfo()),
         Expanded(flex: 2, child: _PomegranadeInfo()),
       ],
     ));
@@ -17,46 +16,88 @@ class ShopInfoScreen extends StatelessWidget {
 }
 
 
-class _BuyInfo extends StatelessWidget {
+class _PomegranadePreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Stack(
       children: [
-         Container(
-           decoration: BoxDecoration(
-           color: Colors.black,
-           border: Border.all(color: Colors.deepPurple[50], width: 10),
-      ),
-  )
-    ],
+        _PinkGranadineBackground(),
+        _BuyInfo(),
+        _AddToCart()
+      ],
     );
   }
 }
 
-class _PomegranadePreview extends StatelessWidget {
+class _AddToCart extends StatelessWidget {
+  const _AddToCart({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: 15,
+        left: 270,
+        child: Container(
+          width: 150,
+          height: 90,
+          color: Colors.green[300],
+        ));
+  }
+}
+
+class _BuyInfo extends StatelessWidget {
+  const _BuyInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: 0,
+        left: 50,
+        child: Container(
+          width: 220,
+          height: 140,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.deepPurple[50], width: 4),
+          ),
+        ));
+  }
+}
+
+class _PinkGranadineBackground extends StatelessWidget {
+  const _PinkGranadineBackground({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
           Container(
-            child: Row(  // Lower this
-            children:[
-            Image.asset("assets/Back arrow icon.png"),
-            Spacer(),
-            Image.asset("assets/Lens icon.png"),
-            Container(width: 60),
-            Image.asset("assets/Store icon.png"),
-            ],
+            child: Row(
+              // Lower this
+              children: [
+                Image.asset("assets/Back arrow icon.png"),
+                Spacer(),
+                Image.asset("assets/Lens icon.png"),
+                Container(width: 60),
+                Image.asset("assets/Store icon.png"),
+              ],
             ),
             margin: EdgeInsets.only(left: 25, top: 60, right: 25, bottom: 0),
           ),
-          Align(alignment: Alignment.topCenter,
-          child: Container(child: Image.asset("assets/Pomegranate image.png")),
+          Align(
+            alignment: Alignment.topCenter,
+            child:
+                Container(child: Image.asset("assets/Pomegranate image.png")),
           ),
-          ],
+        ],
       ),
-
       decoration: BoxDecoration(
           color: Colors.pink.shade100,
           borderRadius: BorderRadius.only(
@@ -71,12 +112,12 @@ class _PomegranadeInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text('UNIT'),
-        ],)
-    );
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('UNIT'),
+          ],
+        ));
   }
 }
